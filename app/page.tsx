48660f1813dece9595e20349788a7828a4742b00
Peter Velkov <kidroca@gmail.com>
2023-05-07 18:31:52 +0300

    feat: favicon and some branding

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main className="flex h-full p-12 items-center">
-      <h1 className="text-3xl font-bold text-center">Добре дошли на това неочаквано място, но то още не е готово за широката публика</h1>
+    <main className="flex flex-col h-full px-12">
+      <h1 className="w-1/2 my-auto mr-auto text-3xl font-bold text-right">Добре дошли на това неочаквано място, но то още не е готово за широката публика</h1>
+      <Image src="/midjourney-castle.png" className="my-auto ml-auto" width={400} height={400} alt="A castle on a flying island" />
     </main>
   )
 }
--- app/stories/generate/page.tsx
+++ app/stories/generate/page.tsx
@@ -6,8 +6,7 @@ import Providers from '@/components/Providers';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image src="/dog.png" className={styles.icon} width={34} height={34} alt="Dog Icon" />
-      <h2>Каква приказка желаете да ви разкажа</h2>
+      <h2>Story Generator</h2>
 
       <Providers>
         <CreateStory />
