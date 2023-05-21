838c1bf136370e54e3322f8ea3315539bec59e9b
Peter Velkov <kidroca@gmail.com>
2023-05-21 17:44:44 +0300

    feat(CreateStory): making title revisions

--- components/CreateStory/CreateStory.tsx
+++ components/CreateStory/CreateStory.tsx
@@ -4,6 +4,7 @@ import { Story } from '@/pages/api/common';
 import StoryContent from '../StoryContent';
 import Wrapper from '@/components/AppShell/Wrapper';
 import BlockEditor from '@/components/TextEditing/BlockEditor';
+import InputGroup from '@/components/TextEditing/InputGroup';
 
 export default function CreateStory() {
   const [result, setResult] = useState<{ story: Story; id: string }>();
@@ -31,17 +32,46 @@ export default function CreateStory() {
     setResult(result);
   }, []);
 
+  const reviseStory = useCallback(
+    (title: string) =>
+      fetch('/api/update', {
+        method: 'PUT',
+        body: JSON.stringify({ story: { title }, id: result!.id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(async (response) => {
+          const json = await response.json();
+          if (!response.ok) {
+            if (Object.keys(json).length > 0) {
+              return Promise.reject(json);
+            }
+
+            throw new Error('Failed to revise story');
+          }
+
+          return json;
+        })
+        .then(() => {
+          setResult((current) => ({
+            ...current!,
+            story: { ...current!.story, title },
+          }));
+        }),
+    [result]
+  );
+
   return (
     <section className="w-full flex flex-col items-center p-2">
       <Wrapper className="flex flex-col my-2">
-        <BlockEditor
-          label="Narrative"
-          submitLabel="Generate"
-          onSubmit={generateStory}
-        />
+        <BlockEditor label="Narrative" submitLabel="Generate" onSubmit={generateStory} />
+
+        {result && (
+          <InputGroup label="Title" defaultValue={result.story.title} onSubmit={reviseStory} />
+        )}
       </Wrapper>
 
-      {result && <h3>{result.story.title}</h3>}
       {result && <StoryContent story={result.story} id={result.id} />}
     </section>
   );
